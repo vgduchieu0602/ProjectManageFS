6ffe2c213331d9daaf09ff968d5b13375405bce3
Vương Đức Hiếu <vg.dhieu0602@gmail.com>
2026-04-01 22:25:26 +0700

    initial project more than

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import "./App.css";
+import Button from "@mui/material/Button";
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
 
 function App() {
   return (
     <>
-      <p>Hello world!</p>
+      <p>Vương Đức Hiếu</p>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <AccessAlarmIcon />
+      <ThreeDRotation />
+      <AcUnitIcon />
     </>
   );
 }
